600f9914632dcdf304d87e805c497fe5cba86a4c
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-06-05 14:07:07 +0530

    feat: created various layout components

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,50 @@
+import {
+  Container,
+  Text,
+  Stack,
+  ButtonGroup,
+  IconButton,
+} from '@chakra-ui/react';
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
+
+export const Footer = () => {
+  return (
+    <Container as="footer" role="contentinfo" marginTop="1rem">
+      <Stack
+        spacing={{
+          base: '4',
+          md: '5',
+        }}
+      >
+        <Stack justify="space-between" direction="column" align="center">
+          <Text fontSize="sm" fontWeight="bold" color="subtle">
+            {`Made by Kedar Kulkarni`}
+          </Text>
+          <ButtonGroup variant="ghost">
+            <IconButton
+              as="a"
+              href="https://www.linkedin.com/in/kedark152"
+              aria-label="LinkedIn"
+              icon={<FaLinkedin fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              href="https://github.com/kedark152"
+              aria-label="GitHub"
+              icon={<FaGithub fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              href="https://twitter.com/Kulkarni12Kedar"
+              aria-label="Twitter"
+              icon={<FaTwitter fontSize="1.25rem" />}
+            />
+          </ButtonGroup>
+          <Text fontSize="sm" color="subtle">
+            &copy; {2022} AgroLife, Inc. All rights reserved.
+          </Text>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+};
