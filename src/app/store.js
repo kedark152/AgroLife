5cb55a23344d07f76249b99cd11aaf02e96c652e
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-06-21 23:23:55 +0530

    added postModal & post Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import postModalReducer from '../features/post/postModalSlice';
+import postReducer from '../features/post/postSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    postModal: postModalReducer,
+    post: postReducer,
   },
 });
