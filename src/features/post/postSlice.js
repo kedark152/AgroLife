cffa44aa7b0c9e5c50bb390abd88979c6fd027c8
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-06-29 23:07:56 +0530

    feat: updated posts slice for updating posts lists for various pages

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { database } from '../../firebase/firebaseConfig';
 import { STATUSES } from '../../utilities/statusesConstants';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, query, where } from 'firebase/firestore';
 import { sortPostsBy } from '../../utilities/utils';
 
 export const getAllPosts = createAsyncThunk(
@@ -11,11 +11,8 @@ export const getAllPosts = createAsyncThunk(
       const querySnapshot = await getDocs(collection(database, 'posts'));
       let posts = [];
       querySnapshot.forEach(doc => {
-        // doc.data() is never undefined for query doc snapshots
         posts.push({ ...doc.data(), postId: doc.id });
-        console.log(doc.id, ' => ', doc.data());
       });
-      console.log(posts);
 
       return sortPostsBy(posts, sortPosts);
     } catch (error) {
@@ -23,10 +20,91 @@ export const getAllPosts = createAsyncThunk(
     }
   }
 );
+export const getHomePosts = createAsyncThunk(
+  'post/getHomePosts',
+  async ({ userId, followingIds, sortPosts }, thunkAPI) => {
+    try {
+      //get all posts
+      const postsRef = collection(database, 'posts');
+      const allPostsSnapshot = await getDocs(postsRef);
+      let allPosts = [];
+      allPostsSnapshot.forEach(doc => {
+        allPosts.push({ ...doc.data(), postId: doc.id });
+      });
 
+      // filter following posts
+      const followingPosts = allPosts.filter(post =>
+        followingIds.some(item => item === post.uid)
+      );
+
+      //filter my posts
+      const currentUserPostsQuery = query(postsRef, where('uid', '==', userId));
+      const querySnapshot = await getDocs(currentUserPostsQuery);
+      let posts = [];
+      querySnapshot.forEach(doc => {
+        posts.push({ ...doc.data(), postId: doc.id });
+      });
+
+      //concatinate following & my posts
+      let homeFeed = posts.concat(followingPosts);
+
+      return sortPostsBy(homeFeed, sortPosts);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+export const getBookmarksPosts = createAsyncThunk(
+  'post/getBookmarksPosts',
+  async ({ bookmarksIds, sortPosts }, thunkAPI) => {
+    try {
+      //get all posts
+      const postsRef = collection(database, 'posts');
+      const allPostsSnapshot = await getDocs(postsRef);
+      let allPosts = [];
+      allPostsSnapshot.forEach(doc => {
+        allPosts.push({ ...doc.data(), postId: doc.id });
+      });
+
+      // filter bookmarks posts
+      const bookmarksPosts = allPosts.filter(post =>
+        bookmarksIds.includes(post.postId)
+      );
+
+      return sortPostsBy(bookmarksPosts, sortPosts);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+export const getSingleUserPosts = createAsyncThunk(
+  'post/getSingleUserPosts',
+  async ({ userId }, thunkAPI) => {
+    try {
+      //get single users posts
+      const postsRef = collection(database, 'posts');
+      const singleUserPostsQuery = query(postsRef, where('uid', '==', userId));
+      const querySnapshot = await getDocs(singleUserPostsQuery);
+      let posts = [];
+      querySnapshot.forEach(doc => {
+        posts.push({ ...doc.data(), postId: doc.id });
+      });
+
+      return sortPostsBy(posts, 'newest');
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 const initialState = {
   allPosts: [],
   allPostsStatus: STATUSES.IDLE,
+  homePosts: [],
+  homePostsStatus: STATUSES.IDLE,
+  bookmarksPosts: [],
+  bookmarksPostsStatus: STATUSES.IDLE,
+  singleUserPosts: [],
+  singleUserPostsStatus: STATUSES.IDLE,
 };
 
 const postSlice = createSlice({
@@ -38,6 +116,60 @@ const postSlice = createSlice({
     },
     setAllPostsStatus: (state, action) => {
       state.allPostsStatus = action.payload;
+    },
+    setHomePosts: (state, action) => {
+      state.homePosts = action.payload;
+    },
+    setHomePostsStatus: (state, action) => {
+      state.homePostsStatus = action.payload;
+    },
+    setBookmarksPosts: (state, action) => {
+      state.bookmarksPosts = action.payload;
+    },
+    likeSingleHomePostState: (state, action) => {
+      //directly updates in state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.homePosts[index].likes =
+        state.homePosts[index].likes.concat(currentUserId);
+    },
+    unLikeSingleHomePostState: (state, action) => {
+      //directly updates in homePosts state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.homePosts[index].likes = state.homePosts[index].likes.filter(
+        id => id !== currentUserId
+      );
+    },
+    likeSingleBookmarksPostState: (state, action) => {
+      //directly updates in bookmarksPosts state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.bookmarksPosts[index].likes =
+        state.bookmarksPosts[index].likes.concat(currentUserId);
+    },
+    unLikeSingleBookmarksPostState: (state, action) => {
+      //directly updates in bookmarksPosts state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.bookmarksPosts[index].likes = state.bookmarksPosts[
+        index
+      ].likes.filter(id => id !== currentUserId);
+    },
+    likeSingleUserPostsState: (state, action) => {
+      //directly updates in singleUserPosts state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.singleUserPosts[index].likes =
+        state.singleUserPosts[index].likes.concat(currentUserId);
+    },
+    unLikeSingleUserPostsState: (state, action) => {
+      //directly updates in singleUserPosts state
+      const index = action.payload.index;
+      const currentUserId = action.payload.currentUserId;
+      state.singleUserPosts[index].likes = state.singleUserPosts[
+        index
+      ].likes.filter(id => id !== currentUserId);
     },
   },
   extraReducers: {
@@ -52,7 +184,54 @@ const postSlice = createSlice({
       console.log('getAllPosts Rejected', action.payload);
       state.allPostsStatus = STATUSES.ERROR;
     },
+    [getSingleUserPosts.pending]: state => {
+      state.singleUserPostsStatus = STATUSES.LOADING;
+    },
+    [getSingleUserPosts.fulfilled]: (state, action) => {
+      state.singleUserPostsStatus = STATUSES.SUCCESS;
+      state.singleUserPosts = action.payload;
+    },
+    [getSingleUserPosts.rejected]: (state, action) => {
+      console.log('get singleUserPosts Rejected', action.payload);
+      state.singleUserPostsStatus = STATUSES.ERROR;
+    },
+    [getHomePosts.pending]: state => {
+      state.homePostsStatus = STATUSES.LOADING;
+    },
+    [getHomePosts.fulfilled]: (state, action) => {
+      state.homePostsStatus = STATUSES.SUCCESS;
+      state.homePosts = action.payload;
+    },
+    [getHomePosts.rejected]: (state, action) => {
+      console.log('get Home posts Rejected', action.payload);
+      state.homePostsStatus = STATUSES.ERROR;
+    },
+    [getBookmarksPosts.pending]: state => {
+      state.bookmarksPostsStatus = STATUSES.LOADING;
+    },
+    [getBookmarksPosts.fulfilled]: (state, action) => {
+      state.bookmarksPostsStatus = STATUSES.SUCCESS;
+      state.bookmarksPosts = action.payload;
+    },
+    [getBookmarksPosts.rejected]: (state, action) => {
+      console.log('getbookmarksPosts Rejected', action.payload);
+      state.bookmarksPostsStatus = STATUSES.ERROR;
+    },
   },
 });
-export const { setAllPosts, setAllPostsStatus } = postSlice.actions;
+export const {
+  setAllPosts,
+  setAllPostsStatus,
+  setHomePosts,
+  setHomePostsStatus,
+  setBookmarksPosts,
+  likeSingleHomePostState,
+  unLikeSingleHomePostState,
+  likeSingleBookmarksPostState,
+  unLikeSingleBookmarksPostState,
+  likeSingleUserPostsState,
+  unLikeSingleUserPostsState,
+} = postSlice.actions;
 export default postSlice.reducer;
+
+//note:idle status not handled after success
