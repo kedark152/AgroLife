595d513c1adb21d28ef4cfb768c34e7729e197a1
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-06-29 23:01:28 +0530

    feat: created toast useEffect hooks for various operations

--- src/hooks/useBookmarkToast.js
+++ src/hooks/useBookmarkToast.js
@@ -1,43 +1,35 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// Posts CRUD Operation Action Toast custom hook
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { useToast } from '@chakra-ui/react';
 import { STATUSES } from '../utilities/statusesConstants';
+import { useSelector, useDispatch } from 'react-redux';
 import { setPostModalStatus } from '../features/post/postModalSlice';
-import { useEffect } from 'react';
-
-export const useCrudToast = () => {
-  const dispatch = useDispatch();
+export const useBookmarkToast = () => {
   const toast = useToast();
+  const dispatch = useDispatch();
   const postModalState = useSelector(state => state.postModal);
 
   useEffect(() => {
-    if (postModalState.statusMessage === STATUSES.POST_CREATED) {
+    if (postModalState.statusMessage === STATUSES.POST_BOOKMARKED) {
       toast({
         title: postModalState.statusMessage,
         status: 'success',
-        duration: 4000,
+        duration: 2000,
         isClosable: true,
       });
       dispatch(setPostModalStatus({ status: STATUSES.IDLE, message: '' }));
     }
-    if (postModalState.statusMessage === STATUSES.POST_DELETED) {
+    if (postModalState.statusMessage === STATUSES.POST_UNBOOKMARKED) {
       toast({
         title: postModalState.statusMessage,
         status: 'success',
-        duration: 4000,
+        duration: 2000,
         isClosable: true,
       });
       dispatch(setPostModalStatus({ status: STATUSES.IDLE, message: '' }));
     }
-    if (postModalState.statusMessage === STATUSES.POST_EDITED) {
-      toast({
-        title: postModalState.statusMessage,
-        status: 'success',
-        duration: 4000,
-        isClosable: true,
-      });
-      dispatch(setPostModalStatus({ status: STATUSES.IDLE, message: '' }));
-    }
-  }, [postModalState.status == STATUSES.SUCCESS]);
+  }, [
+    postModalState.status == STATUSES.SUCCESS_BOOKMARK,
+    postModalState.status == STATUSES.SUCCESS_UNBOOKMARK,
+  ]);
 };
